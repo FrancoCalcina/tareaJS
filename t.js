1750e86afd539903091b490c16fa3aaaa7eac716
FrancoArdiente <francoeslaputaostia@gmail.com>
2020-05-13 17:14:42 -0500

    terminado

--- t.js
+++ t.js
@@ -1,15 +1,21 @@
-let inputNombre=document.getElementById("inputNombre"); 
-let inputFecha=document.getElementById("inputFecha");
-let inputRuc=document.getElementById("inputRu");
-let inputnmr=document.getElementById("inputnmr");
-let input__1=document.getElementById("input__1");
-let input__2=document.getElementById("input__2");
-let input__3=document.getElementById("input__3");
-let input__4=document.getElementById("input__4");
-let carpetita=document.getElementById("carpetita");
-let dinamico=document.getElementById("dinamico");
-let arreglo=[];
-let suma__total=document.getElementById("suma__total");
+let inputNombre = document.getElementById("inputNombre");
+let inputFecha = document.getElementById("inputFecha");
+let inputRuc = document.getElementById("inputRuc");
+let inputnmr = document.getElementById("inputnmro");
+
+let input__1 = document.getElementById("input__1");
+let input__2 = document.getElementById("input__2");
+let input__3 = document.getElementById("input__3");
+let input__4 = document.getElementById("input__4");
+
+let carpetita = document.getElementById("carpetita");
+let dinamico = document.getElementById("dinamico");
+let guardar__todo = document.getElementById("guardar__todo");
+let form = document.getElementById("form");
+
+let arreglo = [];
+let arreglo2 = [];
+let suma__total = document.getElementById("suma__total");
 
 
 
@@ -17,101 +23,117 @@ let suma__total=document.getElementById("suma__total");
 
 const funcionTablo = () => {
     dinamico.innerHTML = "";
-    arreglo.forEach((t) => {
- 
+    arreglo.forEach((e) => {
+
         let fila = document.createElement("tr")
         let cant = document.createElement("td");
         cant.setAttribute("class", "inpùt__1");
-         cant.innerText = t.dato;
-       
+        cant.innerText = e.cantidad;
+
         let des = document.createElement("td");
         des.setAttribute("class", "inpùt__2");
-         des.innerText = t.dato2;
-       
-        let precU= document.createElement("td");
-        
+        des.innerText = e.descripcion;
+
+        let precU = document.createElement("td");
+
         precU.setAttribute("class", "inpùt__3");
-        precU.innerText = t.dato3;
-        
-       
+        precU.innerText = e.unitario;
+
+
         let precT = document.createElement("td");
-        precT.innerHTML= t.dato4; 
-       
+        precT.innerHTML = e.total;
+
         fila.appendChild(cant);
         fila.appendChild(des);
         fila.appendChild(precU);
         fila.appendChild(precT);
-       
+
         dinamico.appendChild(fila);
 
-     
-        
-        
     })
 
-   
 
-   }
-   const sumita=()=>{
+}
+
+const borrarFilas = () => {
+    inputNombre.value = "";
+    inputRuc.value = "";
+    inputFecha.value = "";
+    inputnmr.value = "";
     suma__total.innerHTML = "";
-    let sum=0;
-    let div;
-    let numero;
 
-    arreglo.forEach((e) => {
-        
-        sum+=e.dato4;
-        div = document.createElement("div");
-        numero = document.createElement("p");
-        numero.innerHTML= sum; 
-        
-        div.appendChild(numero);
-        
-        suma__total.appendChild(numero);
-        
-    })
-   }
-
-   input__1.onkeyup = e => {
-    if (e.keyCode === 13) {
-     carpetita.click();
+    for (let i = 0; i <arreglo.length+1; i++) {
+        let fila = document.querySelector("tr");
+        fila.remove();
     }
-   }
-   input__2.onkeyup = e => {
-    if (e.keyCode === 13) {
-     carpetita.click();
+
+
+
+}
+
+//    //////////////////////////////////////////////////////////
+
+input__3.onchange = () => {
+    input__4.value = input__1.value * input__3.value;
+}
+
+input__3.onkeyup = () => {
+    input__4.value = input__1.value * input__3.value;
+}
+
+input__1.onchange = () => {
+    input__4.value = input__1.value * input__3.value;
+}
+
+input__1.onkeyup = () => {
+    input__4.value = input__1.value * input__3.value;
+}
+
+
+let ntotal = 0;
+form.onsubmit = (e) => {
+    e.preventDefault();
+    let objFila = {
+        cantidad: input__1.value,
+        descripcion: input__2.value,
+        unitario: input__3.value,
+        total: input__1.value * input__3.value
     }
-   }
-   input__3.onkeyup = e => {
-    if (e.keyCode === 13) {
-     carpetita.click();
-    }
-   }
 
-   let num=0;
-   carpetita.onclick = () => {
-       
-    let obj = {
-     dato: input__1.value,
-     dato2: input__2.value,
-     dato3: input__3.value,
-     dato4: (input__1.value*input__3.value)
-    }
-    arreglo.push(obj);
-    
 
-    console.log(obj);
-    
+    arreglo.push(objFila);
+    input__1.value = "";
+    input__2.value = "";
+    input__3.value = "";
+    input__4.value = "";
 
-    input__1.value ="";
-    input__2.value ="";
-    input__3.value="";
 
+    ntotal += objFila.total;
+
+  
     funcionTablo();
 
-    sumita();
+    suma__total.innerText = ntotal;
+}
+// /////////////////////////////////////////////////
+guardar__todo.onclick = () => {
 
-   }
-   
-  
+    let facturilla = {
+        nombre: inputNombre.value,
+        ruc: inputRuc.value,
+        fecha: inputFecha.value,
+        nro: inputnmr.value,
+        ntotal,
+        dinamico
+    }
+
+    arreglo2.push(facturilla)
+    let factura = JSON.stringify(arreglo2);
+    localStorage.setItem("arreglo2", factura);
+
+    borrarFilas();
+    arreglo=[];
+    ntotal=0;
+}
+
 
